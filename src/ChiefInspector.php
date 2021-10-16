aa543e959b4f7619d63f748932d6ea2431072822
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-16 09:06:45 +0300

    Adding Kluzo\ChiefInspector

---
+++ src/ChiefInspector.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kluzo;
+
+use Kluzo\Inspector;
+
+use function strtolower;
+
+final class ChiefInspector
+{
+	static private $inspector;
+
+	static function setInspector(Inspector $inspector) : Inspector
+	{
+		return self::$inspector = $inspector;
+	}
+
+	static function getInspector() : Inspector
+	{
+		return self::$inspector ?? (self::$inspector = new Inspector);
+	}
+
+	private const METHOD_ALIAS = array(
+		'ison' => 'isEnabled',
+		'enable' => 'enableInspector',
+		'disable' => 'disableInspector',
+		'on' => 'enablePocket',
+		'off' => 'disablePocket',
+		'clear' => 'cleanPocket',
+	);
+
+	static function __callStatic(string $method, array $args)
+	{
+		if ($alias = self::METHOD_ALIAS[ strtolower($method) ] ?? null)
+		{
+			$method = $alias;
+		}
+
+		return self::getInspector()->$method(...$args);
+	}
+}
