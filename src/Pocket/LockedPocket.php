a5a216158f843bb8bafef0ed68f32d39ee195408
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-23 21:41:21 +0300

    Report using Kluzo\Pocket\LockedPocket::put()

--- src/Pocket/LockedPocket.php
+++ src/Pocket/LockedPocket.php
@@ -6,7 +6,10 @@ use Kluzo\Clue\ClueInterface as Clue;
 use Kluzo\Pocket\PocketInterface as Pocket;
 use Generator;
 
+use const E_USER_WARNING;
+
 use function iterator_count;
+use function trigger_error;
 
 class LockedPocket implements Pocket
 {
@@ -19,6 +22,12 @@ class LockedPocket implements Pocket
 
 	function put(Clue $clue) : Pocket
 	{
+		trigger_error(
+			'You can not put clues into a locked pocket',
+			E_USER_WARNING);
+
+		\Kluzo\Disguise::getInspector()->log('error', 'Can\'t put clues in locked pockets');
+
 		return $this;
 	}
 
