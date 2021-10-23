e409bfc61c0a4ce91cf22244d3d08e448eaea117
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-23 21:37:51 +0300

    Make pocket names case-insensitive

--- src/Pocket/PocketAggregate.php
+++ src/Pocket/PocketAggregate.php
@@ -10,6 +10,7 @@ use Kluzo\Pocket\PocketFactory as DefaultPocketFactory;
 use Generator;
 
 use function array_filter;
+use function strtolower;
 
 class PocketAggregate implements Aggregate
 {
@@ -29,18 +30,21 @@ class PocketAggregate implements Aggregate
 
 	function addPocket(string $pocketName, Pocket $pocketObject) : Aggregate
 	{
+		$pocketName = strtolower($pocketName);
 		$this->pockets[ $pocketName ] = $pocketObject;
 		return $this;
 	}
 
 	function dropPocket(string $pocketName) : Aggregate
 	{
+		$pocketName = strtolower($pocketName);
 		unset($this->pockets[ $pocketName ]);
 		return $this;
 	}
 
 	function cleanPocket(string $pocketName) : Aggregate
 	{
+		$pocketName = strtolower($pocketName);
 		if ($pocket = $this->getPocket( $pocketName ))
 		{
 			$pocket->clean();
@@ -69,23 +73,27 @@ class PocketAggregate implements Aggregate
 
 	function unblockPocket(string $pocketName) : Aggregate
 	{
+		$pocketName = strtolower($pocketName);
 		unset($this->blockedPockets[ $pocketName ]);
 		return $this;
 	}
 
 	function blockPocket(string $pocketName) : Aggregate
 	{
+		$pocketName = strtolower($pocketName);
 		$this->blockedPockets[ $pocketName ] = true;
 		return $this;
 	}
 
 	function isBlocked(string $pocketName) : bool
 	{
+		$pocketName = strtolower($pocketName);
 		return !empty($this->blockedPockets[ $pocketName ]);
 	}
 
 	function getPocket(string $pocketName) : ?Pocket
 	{
+		$pocketName = strtolower($pocketName);
 		if ($this->isBlocked( $pocketName ))
 		{
 			return null;
