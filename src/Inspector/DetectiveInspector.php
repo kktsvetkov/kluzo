b1af0d718d81a1e13942c343d471358800497b33
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-25 12:37:28 +0300

    Change Kluzo\Inspector\DetectiveInspector to use Kluzo\Pocket\StrategyInterface

--- src/Inspector/DetectiveInspector.php
+++ src/Inspector/DetectiveInspector.php
@@ -7,8 +7,11 @@ use Kluzo\Clue\Ignore as IgnoreClue;
 use Kluzo\Clue\Testimony as TestimonyClue;
 
 use Kluzo\Inspector\AbstractInspector as Inspector;
+use Kluzo\Pocket\StrategyInterface as PocketStrategyFinder;
+use Kluzo\Pocket\Strategy\StrategyInterface as PocketStrategy;
+use Kluzo\Pocket\Strategy\CreateStrategy as DefaultPocketStrategy;
 
-class DetectiveInspector extends Inspector
+class DetectiveInspector extends Inspector implements PocketStrategyFinder
 {
 	function log(string $pocketName, ...$things) : Clue
 	{
@@ -17,12 +20,36 @@ class DetectiveInspector extends Inspector
 			return new IgnoreClue;
 		}
 
-		if (!$pocket = $this->pocketAggregate->getPocket( $pocketName ))
+		$pocketAggregate = $this->getPockets();
+		if (!$pocket = $pocketAggregate->getPocket( $pocketName ))
 		{
-			return new IgnoreClue;
+			$pocket = $this->getStrategy()->findPocket(
+				$pocketName,
+				$pocketAggregate
+				);
+
+			if (!$pocket)
+			{
+				return new IgnoreClue;
+			}
 		}
 
 		$pocket->put( $clue = new TestimonyClue(...$things) );
 		return $clue;
+	}
+
+	protected $strategy;
+
+	function setStrategy(PocketStrategy $strategy) : self
+	{
+		$this->strategy = $strategy;
+		return $this;
+	}
+
+	function getStrategy() : ?PocketStrategy
+	{
+		return $this->strategy ?? (
+			$this->strategy = new DefaultPocketStrategy
+			);
 	}
 }
