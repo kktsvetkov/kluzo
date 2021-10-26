82e2b009309f9d557ce9e2e4e465f28e4fa95e74
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-26 12:59:58 +0300

    Adding Kluzo\Kit\Blooper

---
+++ src/Kit/Blooper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Kluzo\Kit;
+
+use Kluzo\Clue\Testimony as TestimonyClue;
+use Kluzo\Disguise as InspectorDisguise;
+use Kluzo\Pocket\ArrayPocket;
+use Kluzo\Pocket\PocketInterface as Pocket;
+
+use const E_USER_WARNING;
+
+use function trigger_error;
+
+class Blooper
+{
+	private static function getBlooperPocket() : ?Pocket
+	{
+		$pocketAggregate = InspectorDisguise::getInspector()->getPockets();
+		if (!$blooperPocket = $pocketAggregate->getPocket('bloopers'))
+		{
+			$pocketAggregate->addPocket(
+				'bloopers',
+				$blooperPocket = new ArrayPocket
+			);
+		}
+
+		return $blooperPocket;
+	}
+
+	static function log(string $blooper)
+	{
+		if (!$blooperPocket = self::getBlooperPocket())
+		{
+			return trigger_error($blooper, E_USER_WARNING);
+		}
+
+		$blooperPocket->put(
+			(new TestimonyClue($blooper))
+				->setLabel('Blooper!')
+				->as('blooper')
+			);
+	}
+}
--- src/Pocket/LockedPocket.php
+++ src/Pocket/LockedPocket.php
@@ -3,15 +3,8 @@
 namespace Kluzo\Pocket;
 
 use Kluzo\Clue\ClueInterface as Clue;
-use Kluzo\Disguise as InspectorDisguise;
+use Kluzo\Kit\Blooper as BlooperKit;
 use Kluzo\Pocket\ArrayPocket;
-use Kluzo\Pocket\PocketInterface as Pocket;
-use Generator;
-
-use const E_USER_WARNING;
-
-use function iterator_count;
-use function trigger_error;
 
 class LockedPocket extends ArrayPocket
 {
@@ -21,29 +14,15 @@ class LockedPocket extends ArrayPocket
 		$this->with('no.index', 'no.count');
 	}
 
-	function put(Clue $clue) : Pocket
+	function put(Clue $clue) : self
 	{
-		trigger_error(
-			'You can not put clues into a locked pocket',
-			E_USER_WARNING);
-
-		InspectorDisguise::getInspector()->log(
-			'bloopers', 'Can\'t put clues in locked pockets'
-			)->setLabel('Blooper!')->as('blooper');
-
+		BlooperKit::log('Can\'t put clues in locked pockets');
 		return $this;
 	}
 
-	function clean() : Pocket
+	function clean() : self
 	{
-		trigger_error(
-			'You can not clean a locked pocket',
-			E_USER_WARNING);
-
-		InspectorDisguise::getInspector()->log(
-			'bloopers', 'Can\'t clean locked pockets'
-			)->setLabel('Blooper!')->as('blooper');
-
+		BlooperKit::log('Can\'t clean locked pockets');
 		return $this;
 	}
 
