19924b412b2447488f20e41858c472ea441a8912
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-26 12:41:32 +0300

    Adding Kluzo\Kit\Dump::var_dump()

--- src/Kit/Dump.php
+++ src/Kit/Dump.php
@@ -3,10 +3,17 @@
 namespace Kluzo\Kit;
 
 use function print_r;
+use function ob_start;
+use function ob_get_clean;
+use function var_dump;
 
 class Dump
 {
-	static protected $dumpCallback = '\\Kluzo\\Kit\\Dump::print_r';
+	const PRINT_R = '\\Kluzo\\Kit\\Dump::print_r';
+
+	const VAR_DUMP = '\\Kluzo\\Kit\\Dump::var_dump';
+
+	static protected $dumpCallback = self::PRINT_R;
 
 	function dumpWith(callable $dumpCallback) : callable
 	{
@@ -15,14 +22,21 @@ class Dump
 		return $old;
 	}
 
-	static function print_r($thing) : string
-	{
-		return print_r($thing, true);
-	}
-
 	static function dump($thing) : string
 	{
 		return (self::$dumpCallback)($thing);
+	}
+
+	static function print_r($thing) : string
+	{
+		return print_r($thing, true) . "\n";
+	}
+
+	static function var_dump($thing) : string
+	{
+		ob_start();
+		var_dump($thing);
+		return ob_get_clean();
 	}
 
 }
